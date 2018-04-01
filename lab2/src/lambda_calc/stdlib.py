5d7d42d84f1449b5430552104f2e501a4ea4ee95
Shachar Itzhaky <corwin.amber@gmail.com>
2018-04-01 23:51:09 +0300

    FEATURE: Template for assignment. FEATURE: Types of standard constants.

---
+++ lab2/src/lambda_calc/stdlib.py
@@ -0,0 +1,15 @@
+from .syntax import LambdaParser
+
+
+
+DECLARATIONS = """
+    \(succ prev : int -> int) (plus mult pow minus : int -> int -> int)
+     (True False : bool) (not : bool -> bool) (and or xor : bool -> bool -> bool)
+     (is_zero : int -> bool)
+    . dummy
+"""
+
+
+CONSTANTS = {decl.subtrees[0].subtrees[0].root: decl.subtrees[1] 
+             for decl in LambdaParser()(DECLARATIONS).split()
+             if decl.root == ':'}
