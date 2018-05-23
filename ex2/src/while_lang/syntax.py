9931e6417a7ef6da3fdbd541c5e891c4b82b3946
Shachar Itzhaky <corwin.amber@gmail.com>
2018-05-23 11:41:47 +0300

    Parse ints in While parser.

--- ex2/src/while_lang/syntax.py
+++ ex2/src/while_lang/syntax.py
@@ -7,13 +7,14 @@ from parsing.silly import SillyLexer
 
 class WhileParser(object):
 
-    TOKENS = r"(if|then|else|while|do|skip)(?![\w\d_])   (?P<id>[^\W\d]\w*)   (?P<num>\d+)   (?P<op>!=|<=|>=|([+\-*/<>=]))    [();]  :=".split()
+    TOKENS = r"(if|then|else|while|do|skip)(?![\w\d_])   (?P<id>[^\W\d]\w*)   (?P<num>[+\-]?\d+)   (?P<op>[!<>]=|([+\-*/<>=]))    [();]  :=".split()
     GRAMMAR = r"""
     S   ->   S1     |   S1 ; S
     S1  ->   skip   |   id := E   |   if E then S else S1   |   while E do S1
     S1  ->   ( S )
-    E   ->   E0 op E0  |  E0
-    E0  ->   id | num | ( E )
+    E   ->   E0   |   E0 op E0
+    E0  ->   id   |   num
+    E0  ->   ( E )
     """
     
     def __init__(self):
@@ -42,6 +43,8 @@ class WhileParser(object):
             return self.postprocess(t.subtrees[1])
         elif t.root == 'S1' and t.subtrees[0].root in ['if', 'while']:
             return self.postprocess(Tree(t.subtrees[0].root, t.subtrees[1::2]))
+        elif t.root == 'num':
+            return Tree(t.root, [Tree(int(t.subtrees[0].root))])  # parse ints
 
         return Tree(t.root, [self.postprocess(s) for s in t.subtrees])
     
