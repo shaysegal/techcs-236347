b3f1c571aed1caafc59fb740f32fb181d5c455e4
Shachar Itzhaky <corwin.amber@gmail.com>
2018-05-24 20:01:19 +0300

    "Starter" template for ex2.

---
+++ ex2/src/while_lang/wp.py
@@ -0,0 +1,60 @@
+from while_lang.syntax import WhileParser
+import operator
+from z3 import Int, ForAll, Implies, Not, And, Or, Solver, unsat, sat
+
+
+
+OP = {'+': operator.add, '-': operator.sub, 
+      '*': operator.mul, '/': operator.floordiv,
+      '!=': operator.ne, '>': operator.gt, '<': operator.lt,
+      '<=': operator.le, '>=': operator.ge, '=': operator.eq}
+
+
+def mk_env(pvars):
+    return {v : Int(v) for v in pvars}
+
+def upd(d, k, v):
+    d = d.copy()
+    d[k] = v
+    return d
+
+
+
+
+if __name__ == '__main__':
+    # example program
+    pvars = ['a', 'b', 'i', 'n']
+    program = "a := b ; while i < n do ( a := a + 1 ; b := b + 1 )"    
+    P = lambda _: True
+    Q = lambda d: d['a'] == d['b']
+    linv = lambda d: d['a'] == d['b']
+    
+    #
+    # Following are other programs that you might want to try
+    #
+
+    ## Program 1        
+    #pvars = ['x', 'i', 'y']
+    #program = "y := 0 ; while y < i do ( x := x + y ; if (x * y) < 10 then y := y + 1 else skip )"
+    #P = lambda d: d['x'] > 0
+    #Q = lambda d: d['x'] > 0
+    #linv = lambda d: **figure it out!**
+
+    ## Program 2
+    #pvars = ['a', 'b']
+    #program = "while a != b do if a > b then a := a - b else b := b - a"
+    #P = lambda d: And(d['a'] > 0, d['b'] > 0)
+    #Q = lambda d: And(d['a'] > 0, d['a'] == d['b'])
+    #linv = lambda d: ???
+
+
+
+    ast = WhileParser()(program)
+    
+    if ast:
+        print(">> Valid program.")
+        # Now something like:
+        # verify(P, ast, Q)
+    else:
+        print(">> Invalid program.")
+
