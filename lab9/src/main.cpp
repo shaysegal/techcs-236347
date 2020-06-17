b9021b228e7b5e5addcf996f33f28ec80872f6cf
Shachar Itzhaky <corwin.amber@gmail.com>
2020-06-17 17:51:17 +0300

    [bugfix] Use arrays for scanner input.

--- lab9/src/main.cpp
+++ lab9/src/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]) {
     bool result;
     while(1) {
         std::getline(std::cin, s);
+        if (!std::cin.good()) break;
         ANONYMOUS::match(s.size(), (char*)s.c_str(), result);
         std::cout << " = " << result << std::endl;
     }
