590421c35e61385a0d248478faa9c1113a8d6b1d
itay <itayt23@gmail.com>
2023-09-10 18:59:48 +0300

    added some json examples to check code

--- project/src/while_lang/main.py
+++ project/src/while_lang/main.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as sg
 from window import Layout
+import os
+import json
 
 window = Layout().getMainLayout()
 
@@ -25,8 +27,44 @@ def process_user_input():
     window.close()
 
 if __name__ == '__main__':
-    process_user_input()
+    # process_user_input()
     # symbols = ['MSFT','NVDA']
     # for sym in symbols:
     #     pass
-        
+
+    # Define the directory path where your JSON files are located
+    directory_path = 'examples/simple_pbe/'
+
+    # Initialize an empty dictionary to store the data
+    data_dict = {}
+
+    # Loop through the files in the directory
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            # Check if the file ends with ".json"
+            if file.endswith(".json"):
+                # Construct the full file path
+                file_path = os.path.join(root, file)
+                
+                # Open and read the JSON file
+                with open(file_path, 'r') as json_file:
+                    # Load the JSON data into a dictionary
+                    json_data = json.load(json_file)
+                    
+                    # Extract the relevant information
+                    program = json_data['program']
+                    linv = json_data['linv']
+                    pvars = json_data['pvars']
+                    P = json_data['P']
+                    Q = json_data['Q']
+                    
+                    # Store the data in the data_dict using a hierarchical structure
+                    data_dict.setdefault('examples', {}).setdefault('simple_pbe', {})[file] = {
+                        'program': program,
+                        'linv': linv,
+                        'pvars': pvars,
+                        'P': P,
+                        'Q': Q
+                    }
+
+            
