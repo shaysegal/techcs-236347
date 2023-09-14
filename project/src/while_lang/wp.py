1e4cbd28bbff8a8886f69bea3847535477d78139
shaysegal <siglons@gmail.com>
2023-09-14 20:01:04 +0300

    limit smy solver with 2 secs

--- project/src/while_lang/wp.py
+++ project/src/while_lang/wp.py
@@ -234,6 +234,7 @@ def send_to_synt_pbe(values_array,post_id,env,template):
                 sol.add(val == z3_lut[key])
         if not should_check:
             continue
+        sol.set("timeout", 2000)
         status = sol.check()
         if status == sat:
             m = sol.model()
